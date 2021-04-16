462ba38b7a4d90a2cdd988787018cb64a4833701
SwatejPatil <81546709+SwatejPatil@users.noreply.github.com>
2021-04-16 21:45:26 +0530

    Added lines

--- main.py
+++ main.py
@@ -4,13 +4,28 @@ pygame.init()
 
 WIDTH = 600
 HEIGHT = 600
+LINE_WIDTH = 25
 RED = (255, 0, 0)
 BG_COLOR = (30, 160, 170)
+LINE_COLOR=(25, 15, 200)
 
 
-sceen =pygame.display.set_mode((WIDTH, HEIGHT))
+screen =pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('TIC TAC TOE')
-sceen.fill(BG_COLOR)
+screen.fill(BG_COLOR)
+
+
+def draw_lines():8
+    #1st horizontal line
+    pygame.draw.line(screen, LINE_COLOR, (0, 200), (600, 200), LINE_WIDTH)
+    #2nd horizontal line
+    pygame.draw.line(screen, LINE_COLOR, (0, 400), (600, 400), LINE_WIDTH)
+    #1st vertical line
+    pygame.draw.line(screen, LINE_COLOR, (200, 0), (200, 600), LINE_WIDTH)
+    #2nd vertical line
+    pygame.draw.line(screen, LINE_COLOR, (400, 0), (400, 600), LINE_WIDTH)
+
+draw_lines()
 #Main Game LOOP
 while True:
     for event in pygame.event.get():
