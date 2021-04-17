ecca47ae2379e029f159c45f056a6843ce725c9d
SwatejPatil <81546709+SwatejPatil@users.noreply.github.com>
2021-04-17 11:00:57 +0530

    Added board

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame, sys
-
+import numpy as np
 pygame.init()
 
+BOARD_ROWS = 3
+BOARD_COLS = 3 
 WIDTH = 600
 HEIGHT = 600
 LINE_WIDTH = 25
@@ -14,8 +16,12 @@ screen =pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('TIC TAC TOE')
 screen.fill(BG_COLOR)
 
+#board
+board = np.zeros((BOARD_ROWS, BOARD_COLS))
+print(board)
 
-def draw_lines():8
+
+def draw_lines():
     #1st horizontal line
     pygame.draw.line(screen, LINE_COLOR, (0, 200), (600, 200), LINE_WIDTH)
     #2nd horizontal line
@@ -24,6 +30,26 @@ def draw_lines():8
     pygame.draw.line(screen, LINE_COLOR, (200, 0), (200, 600), LINE_WIDTH)
     #2nd vertical line
     pygame.draw.line(screen, LINE_COLOR, (400, 0), (400, 600), LINE_WIDTH)
+
+def mark_square(row, col, player):
+    board[row][col] = player
+
+def available_square(row, col):
+    return board[row][col] == 0
+
+def is_board_full():
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if board[row][col] == 0:
+                return False
+    return True
+
+print(is_board_full())
+for row in range(BOARD_ROWS):
+    for col in range(BOARD_COLS):
+        mark_square(row, col, 1)
+print(is_board_full())
+
 
 draw_lines()
 #Main Game LOOP
